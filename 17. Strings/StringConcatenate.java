8937848730308aee3210c637faf142053b0266e7
Subodh Rakshit <subodh.rakshit@precisely.com>
2023-06-03 09:48:47 +0530

    string concatenation code

---
+++ 17. Strings/StringConcatenate.java
@@ -0,0 +1,9 @@
+public class StringConcatenate {
+    public static void main(String[] args) {
+        // Concatenation
+        String firstName = "Subodh";
+        String lastName = "Rakshit";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+    }
+}
