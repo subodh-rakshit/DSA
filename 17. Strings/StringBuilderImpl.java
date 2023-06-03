a4fb23f2210a95450cd6aa680f98413bd56dcdb1
Subodh Rakshit <subodh.rakshit@precisely.com>
2023-06-03 10:56:51 +0530

    string builder impl

---
+++ 17. Strings/StringBuilderImpl.java
@@ -0,0 +1,11 @@
+public class StringBuilderImpl {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("");
+        for(char ch = 'a'; ch <= 'z'; ch++){
+            sb.append(ch);
+        }
+        // O(26)
+        System.out.println(sb);
+        System.out.println(sb.length());
+    }
+}
