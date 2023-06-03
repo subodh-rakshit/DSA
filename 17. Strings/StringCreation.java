b4d4c80c8b10af74d8b09930442cb78aab1560aa
Subodh Rakshit <subodh.rakshit@precisely.com>
2023-06-03 09:46:25 +0530

    string creation, input and length

---
+++ 17. Strings/StringCreation.java
@@ -0,0 +1,9 @@
+public class StringCreation {
+   public static void main(String[] args) {
+        char arr[] = {'a', 'b', 'c', 'd'};
+        String str = "abcd";
+        String str2 = new String("xyz");
+
+        // Strings are IMMUTABLE
+   } 
+}
---
+++ 17. Strings/StringLength.java
@@ -0,0 +1,6 @@
+public class StringLength{
+    public static void main(String[] args) {
+        String fullName = "Subodh Rakshit";
+        System.out.println(fullName.length());
+    }
+}
