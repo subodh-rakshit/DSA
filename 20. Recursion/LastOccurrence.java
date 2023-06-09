a383997844031edbdbb5cae0a041b03371956b05
Subodh Rakshit <subodh.rakshit@precisely.com>
2023-06-09 20:17:09 +0530

    last occurrence

---
+++ 20. Recursion/LastOccurrence.java
@@ -0,0 +1,20 @@
+public class LastOccurrence {
+
+    public static int lastOccurrence(int arr[], int key, int i) {
+        if(i == arr.length){
+            return -1;
+        }
+        int isFound = lastOccurrence(arr, key, i + 1);
+
+        if(isFound == -1 && arr[i] == key){
+            return i;
+        }
+
+        return isFound;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 8, 3, 6, 9, 5, 10, 2, 5, 3 };
+        System.out.println(lastOccurrence(arr, 5, 0));
+    }
+}
