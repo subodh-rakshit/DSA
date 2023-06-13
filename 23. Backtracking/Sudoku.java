7222b7a1d3926d83a15d82b044df8836f77231c7
Subodh Rakshit <subodh.rakshit@precisely.com>
2023-06-13 20:17:42 +0530

    sudoku solution

---
+++ 23. Backtracking/Sudoku.java
@@ -0,0 +1,93 @@
+public class Sudoku {
+
+  public static boolean isSafe(int sudoku[][], int row, int col, int digit){
+    // column
+    for(int i = 0; i <= 8; i++){
+      if(sudoku[i][col] == digit){
+        return false;
+      }
+    }
+
+    // row
+    for(int j = 0; j <= 8; j++){
+      if(sudoku[row][j] == digit){
+        return false;
+      }
+    }
+
+    // grid
+    int sr = (row / 3) * 3;
+    int sc = (col / 3) * 3;
+
+    // 3 X 3 grid
+    for(int i = sr; i < sr + 3; i++){
+      for(int j = sc; j < sc + 3; j++){
+        if(sudoku[i][j] == digit){
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  public static boolean sudoku_solver(int sudoku[][], int row, int col){
+    // base case
+    if(row == 9){
+      return true;
+    }
+
+    // recursion
+    int nextRow = row, nextCol = col + 1;
+    if(col + 1 == 9){
+      nextRow = row + 1;
+      nextCol = 0;
+    }
+
+    // check if the current position already contains a value > 0 then we iterate through next column
+    if(sudoku[row][col] != 0){
+      return sudoku_solver(sudoku, nextRow, nextCol);
+    }
+
+    for(int digit = 1; digit <= 9; digit++){
+      // check if it is safe to place the num (1 - 9) in the given row, col -> we move to next column
+      if(isSafe(sudoku, row, col, digit)){
+        sudoku[row][col] = digit;
+        if(sudoku_solver(sudoku, nextRow, nextCol)){  // solution exists
+          return true;
+        }
+        sudoku[row][col] = 0;
+      }
+    }
+
+    return false;
+  }
+
+  public static void printSudoku(int sudoku[][]){
+    for(int i = 0; i < 9; i++){
+      for(int j = 0; j < 9; j++){
+        System.out.print(sudoku[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+
+  public static void main(String[] args) {
+    int sudoku[][] = { { 0, 0, 8, 0, 0, 0, 0, 0, 0 },
+        { 4, 9, 0, 1, 5, 7, 0, 0, 2 },
+        { 0, 0, 3, 0, 0, 4, 1, 9, 0 },
+        { 1, 8, 5, 0, 6, 0, 0, 2, 0 },
+        { 0, 0, 0, 0, 2, 0, 0, 6, 0 },
+        { 9, 6, 0, 4, 0, 5, 3, 0, 0 },
+        { 0, 3, 0, 0, 7, 2, 0, 0, 4 },
+        { 0, 4, 9, 0, 3, 0, 0, 5, 7 },
+        { 8, 2, 7, 0, 0, 9, 0, 1, 3 } };
+        
+        if(sudoku_solver(sudoku, 0, 0)){
+          System.out.println("Solution exists");
+          printSudoku(sudoku);
+        } else {
+          System.out.println("Solution does not exist");
+        }
+  }
+}
