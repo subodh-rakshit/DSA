df49a293224ca56cd3d28b76c1e987c149162db6
Subodh Rakshit <subodh.rakshit@precisely.com>
2023-06-13 20:37:14 +0530

    introduction to arraylist

---
+++ 24. ArrayLists/IntroductionToArrayLists.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class IntroductionToArrayLists {
+  public static void main(String[] args) {
+    // ClassName objectName = new ClassName();
+    ArrayList<Integer> list = new ArrayList<>();
+    ArrayList<String> list2 = new ArrayList<>();
+    ArrayList<Boolean> list3 = new ArrayList<>();
+  }
+}
