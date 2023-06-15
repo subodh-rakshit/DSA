0b7f358d8ab91b7c5e95d78948d465c7eb0cb1cd
Subodh Rakshit <subodh.rakshit@precisely.com>
2023-06-15 12:59:22 +0530

    remove cycle in linked list

--- 26. Linked Lists (Part 2)/LinkedListPart2.java
+++ 26. Linked Lists (Part 2)/LinkedListPart2.java
@@ -268,6 +268,36 @@ public class LinkedListPart2 {
     return false;
   }
 
+  public static void removeCycle() {
+    // detect cycle
+    Node slow = head;
+    Node fast = head;
+    boolean cycle = false;
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+      if (fast == slow) {
+        cycle = true;
+        break;
+      }
+    }
+    if (cycle == false) {
+      return;
+    }
+
+    // find meeting point
+    slow = head;
+    Node prev = null;   // last node track
+    while (slow != fast) {
+      prev = fast;
+      slow = slow.next;
+      fast = fast.next;
+    }
+
+    // remove cycle -> last.next = null
+    prev.next = null;
+  }
+
   public static void main(String[] args) {
 
     // Detect Cycle in LL
@@ -277,10 +307,21 @@ public class LinkedListPart2 {
     // head.next.next.next = head;
     // 1-> 2 -> 3 -> 1
 
-    head = new Node(1);
-    head.next = new Node(2);
-    head.next.next = new Node(3);
+    // head = new Node(1);
+    // head.next = new Node(2);
+    // head.next.next = new Node(3);
 
+    // System.out.println(isCycle());
+
+    // Remove Cycle in LinkedList
+    head = new Node(1);
+    Node temp = new Node(2);
+    head.next = temp;
+    head.next.next = new Node(3);
+    head.next.next.next = temp;
+    // 1-> 2 -> 3 -> 2
+    System.out.println(isCycle());
+    removeCycle();
     System.out.println(isCycle());
   }
 }
