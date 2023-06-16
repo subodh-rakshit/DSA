304be495a5aa4d06a4a03a7e44d05cab86d86617
Subodh Rakshit <subodh.rakshit@precisely.com>
2023-06-16 20:56:09 +0530

    duplicate parentheses using stack

---
+++ 27. Stack/DuplicateParentheses.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+public class DuplicateParentheses {
+
+  public static boolean isDuplicate(String str) {
+    Stack<Character> s = new Stack<>();
+
+    for (int i = 0; i < str.length(); i++) {
+      char ch = str.charAt(i);
+
+      // case 1: closing case
+      if (ch == ')') {
+        int count = 0;
+        while (!s.isEmpty() && s.peek() != '(') {
+          s.pop();
+          count++;
+        }
+        if (count < 1) {
+          return true; // duplicate exists
+        } else {
+          s.pop(); // opening pair
+        }
+      } else {
+        // case 2: opening, operator, operands
+        s.push(ch);
+      }
+    }
+    return false;
+  }
+
+  public static void main(String[] args) {
+
+    // Valid String
+    String str = "((a+b))"; // true
+    String str2 = "(a-b)"; // false
+
+    System.out.println(isDuplicate(str));
+    System.out.println(isDuplicate(str2));
+  }
+}
