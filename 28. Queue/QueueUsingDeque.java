529b052bacf88dcb5e3909f5e4f07b7e202b9cf9
Subodh Rakshit <subodh.rakshit@precisely.com>
2023-06-17 19:11:37 +0530

    queue using deque

---
+++ 28. Queue/QueueUsingDeque.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.util.LinkedList;
+
+public class QueueUsingDeque {
+  static class Queue {
+    Deque<Integer> deque = new LinkedList<>();
+
+    // add
+    public void add(int data) {
+      deque.addLast(data);
+    }
+
+    // remove
+    public int remove() {
+      return deque.removeFirst();
+    }
+
+    // peek
+    public int peek() {
+      return deque.getFirst();
+    }
+  }
+
+  public static void main(String[] args) {
+    Queue q = new Queue();
+    q.add(1);
+    q.add(2);
+    q.add(3);
+
+    System.out.println("peek = " + q.peek());
+    System.out.println(q.remove());
+    System.out.println(q.remove());
+    System.out.println(q.remove());
+
+  }
+}
