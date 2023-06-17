415ddacf337beac56360d3f7f7f2a3b74b743333
Subodh Rakshit <subodh.rakshit@precisely.com>
2023-06-17 17:05:46 +0530

    Deque Methods

---
+++ 28. Queue/DequeMethods.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.LinkedList;
+
+public class DequeMethods {
+
+  public static void main(String[] args) {
+    Deque<Integer> deque = new LinkedList<>();
+    deque.addFirst(1); // 1
+    deque.addFirst(2); // 2 1
+    deque.addLast(3); // 2 1 3
+    deque.addLast(4); // 2 1 3 4
+
+    System.out.println(deque);
+    deque.removeFirst();
+    System.out.println(deque);
+    deque.removeLast();
+    System.out.println(deque);
+    System.out.println("first element: " + deque.getFirst());
+    System.out.println("last element: " + deque.getLast());
+  }
+}
