0494de81670cc85c957cfef5b191b147b0e94e7e
Subodh Rakshit <subodh.rakshit@precisely.com>
2023-07-05 00:25:31 +0530

    heap sort for ascending and descending order

---
+++ 35. Heaps/HeapSortAscending.java
@@ -0,0 +1,51 @@
+public class HeapSortAscending {
+
+  public static void heapify(int i, int size, int arr[]){
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+    int maxIdx = i;
+    if(left < size && arr[left] > arr[maxIdx]){
+      maxIdx = left;
+    }
+
+    if(right < size && arr[right] > arr[maxIdx]){
+      maxIdx = right;
+    }
+
+    if(maxIdx != i){
+      // swap
+      int temp = arr[i];
+      arr[i] = arr[maxIdx];
+      arr[maxIdx] = temp;
+
+      heapify(maxIdx, size, arr);
+    }
+  }
+
+  public static void heapSort(int arr[]){
+    // step 1 - build maxHeap
+    int n = arr.length;
+    for(int i = n /2; i >= 0; i--){
+      heapify(i, n, arr);
+    }
+
+    // step 2 - push largest at end
+    for(int i = n - 1; i > 0; i--){
+      // swap (largest - first with last)
+      int temp = arr[0];
+      arr[0] = arr[i];
+      arr[i] = temp;
+      heapify(0, i, arr);
+    }
+  }
+  public static void main(String[] args) {
+    int arr[] = { 1, 2, 4, 5, 3 };
+    heapSort(arr);
+
+    // print
+    for(int i = 0; i < arr.length; i++){
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
+}
