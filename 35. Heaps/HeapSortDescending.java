ad2030d75e31b41312ecc9bf272bf96a3797ee3d
Subodh Rakshit <subodh.rakshit@precisely.com>
2023-07-05 00:26:53 +0530

    heap sort comments

--- 35. Heaps/HeapSortDescending.java
+++ 35. Heaps/HeapSortDescending.java
@@ -1,18 +1,18 @@
 public class HeapSortDescending {
 
-  public static void heapify(int i, int size, int arr[]){
+  public static void heapify(int i, int size, int arr[]) {
     int left = 2 * i + 1;
     int right = 2 * i + 2;
     int maxIdx = i;
-    if(left < size && arr[left] < arr[maxIdx]){
+    if (left < size && arr[left] < arr[maxIdx]) {
       maxIdx = left;
     }
 
-    if(right < size && arr[right] < arr[maxIdx]){
+    if (right < size && arr[right] < arr[maxIdx]) {
       maxIdx = right;
     }
 
-    if(maxIdx != i){
+    if (maxIdx != i) {
       // swap
       int temp = arr[i];
       arr[i] = arr[maxIdx];
@@ -22,15 +22,15 @@ public class HeapSortDescending {
     }
   }
 
-  public static void heapSort(int arr[]){
+  public static void heapSort(int arr[]) {
     // step 1 - build maxHeap
     int n = arr.length;
-    for(int i = n /2; i >= 0; i--){
+    for (int i = n / 2; i >= 0; i--) { // O(nlogn)
       heapify(i, n, arr);
     }
 
     // step 2 - push largest at end
-    for(int i = n - 1; i > 0; i--){
+    for (int i = n - 1; i > 0; i--) { // O(nlogn)
       // swap (largest - first with last)
       int temp = arr[0];
       arr[0] = arr[i];
@@ -38,12 +38,13 @@ public class HeapSortDescending {
       heapify(0, i, arr);
     }
   }
+
   public static void main(String[] args) {
     int arr[] = { 1, 2, 4, 5, 3 };
     heapSort(arr);
 
     // print
-    for(int i = 0; i < arr.length; i++){
+    for (int i = 0; i < arr.length; i++) {
       System.out.print(arr[i] + " ");
     }
     System.out.println();
