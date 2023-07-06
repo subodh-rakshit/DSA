06d33c123bca8c6710001927259ef056e7003ea9
Subodh Rakshit <subodh.rakshit@precisely.com>
2023-07-07 02:18:54 +0530

    union and intersection of arrays using hashset code

---
+++ 36. Hashing/UnionAndIntersection.java
@@ -0,0 +1,40 @@
+import java.util.HashSet;
+
+public class UnionAndIntersection {
+  public static void main(String[] args) {
+    int arr1[] = { 7, 3, 9 };
+    int arr2[] = { 6, 3, 9, 2, 9, 4 };
+    HashSet<Integer> set = new HashSet<>();
+
+    // union
+    for (int i = 0; i < arr1.length; i++) {
+      set.add(arr1[i]);
+    }
+
+    for (int i = 0; i < arr2.length; i++) {
+      set.add(arr2[i]);
+    }
+
+    System.out.println("union = " + set.size());
+
+
+    // intersection
+    set.clear();
+
+    // adding arr1 elements into the set
+    for (int i = 0; i < arr1.length; i++) {
+      set.add(arr1[i]);
+    }
+
+    int count = 0;
+    // traversing in arr2
+    for(int i = 0; i < arr2.length; i++){
+      if(set.contains(arr2[i])){
+        count++;
+        set.remove(arr2[i]);
+      }
+    }
+
+    System.out.println("intersection = "+count);
+  }
+}
